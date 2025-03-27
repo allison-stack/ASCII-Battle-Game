5fd03ecd08f1e04c93e416189d1614035197e4e4
Allison Zhao <allisonzhao.uni@gmail.com>
2025-03-27 00:17:32 -0400

    modify receiverThread such that it follows code template and quits without throwing an exception

--- client.py
+++ client.py
@@ -23,20 +23,24 @@ g_serverSocket = None  # shared by main thread and receiver thread
 ###############################################################################
 # TODO: continuously receive updates (ASCII grid) from the server (Involves the buffer i think)
 ###############################################################################
-def receiverThread(kill: threading.Event):
+def receiverThread():
     global g_serverSocket
-    while not kill.is_set():
-        # Buffer from server to check if game is ending
-        tr,_,_ = select.select([g_serverSocket], [], [], 0)
-        if tr:
-            data = g_serverSocket.recv(BUFFER_SIZE).decode()
-
-            # TODO: implement end of game on server side
-            if "Game over" in data:
+    while True:
+        try:
+            data = g_serverSocket.recv(BUFFER_SIZE)
+            if not data:
+                print("Server disconnected")
                 break
+            print(data.decode('utf-8'))
+        except ConnectionResetError:
+            print("Connection to server lost")
+            break
+        except:
+            print("Error receiving data")
+            break
 
-    # g_serverSocket.close()
-    # sys.exit(0)
+    g_serverSocket.close()
+    sys.exit(0)
 
 ###############################################################################
 # main: connect to server, spawn receiver thread, send commands in a loop
@@ -63,8 +67,8 @@ def main():
 
     # Spawn receiver thread
     # TODO: Implement threading for client 
-    kill = threading.Event()
-    t = threading.Thread(target=receiverThread, args=(kill,), daemon=True)
+    t = threading.Thread(target=receiverThread)
+    t.daemon = True
     t.start()
 
     # Main loop: read commands, send to server
@@ -84,15 +88,13 @@ def main():
                 continue   
         
         # TODO: send command to server
-        g_serverSocket.sendall(cmd.encode('utf-8').ljust(BUFFER_SIZE, b' '))
+        g_serverSocket.sendall(cmd.encode('utf-8'))
 
         # If QUIT => break
         if cmd.upper().startswith("QUIT"):
             break
 
     # cleanup
-    kill.set()
-    t.join()
     if g_serverSocket:
         g_serverSocket.close()
     sys.exit(0)
